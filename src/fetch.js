08e5a96f296f240d3be883f68a3d3f12dcaad75f
Lucid Macbook <Lucid@Lucids-MacBook-Pro.local>
2024-10-30 14:58:02 +0000

    Add : Filter projects by published at

--- src/fetch.js
+++ src/fetch.js
@@ -2,44 +2,44 @@ import client from '@src/sanityClient.js';
 
 // Function to fetch all posts/projects
 export const fetchPosts = async () => {
-    const query = `*[_type == "post"]{
-      _id,
-      title,
-      slug,
-      description,
-      categories[]->{
-        title
-      },
-      project_type,
-      author->{
-        name
-      },
-      main_image{
-        asset->{
-          url
-        }
-      },
-      iframe_status,
-      iframe_height,
-      iframe_width,
-      iframe_url,
-      iframe_src,
-      published_at,
-      stories[]{
-        tab,
-        content
+  const query = `*[_type == "post"] | order(published_at desc){
+    _id,
+    title,
+    slug,
+    description,
+    categories[]->{
+      title
+    },
+    project_type,
+    author->{
+      name
+    },
+    main_image{
+      asset->{
+        url
       }
-    }`;
-  
-    try {
-      const posts = await client.fetch(query);
-      // console.log("Fetching post");
-      return posts;
-    } catch (error) {
-      console.error('Error fetching posts:', error);
-      return [];
+    },
+    iframe_status,
+    iframe_height,
+    iframe_width,
+    iframe_url,
+    iframe_src,
+    published_at,
+    stories[]{
+      tab,
+      content
     }
-  };
+  }`;
+
+  try {
+    const posts = await client.fetch(query);
+    return posts;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return [];
+  }
+};
+
 
 
   // Funciton to fetch project categories eg. excel, python, timeseries
